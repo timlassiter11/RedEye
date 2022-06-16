315365559be74559800c3c84605e68a85dc14c5a
Timothy Lassiter <timlassiter11@gmail.com>
2022-06-16 15:16:00 -0400

    Fixed api not returning correct response

--- app/api/helpers.py
+++ app/api/helpers.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 from flask import jsonify
 from flask_login import current_user
 from flask_restful import abort
@@ -10,24 +12,28 @@ def json_abort(status_code, **kwargs):
 
 
 def login_required(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
         if current_user.is_anonymous:
-            json_abort(401, message=['Not authorized'])
-        func(*args, **kwargs)
+            json_abort(401, message="Not authorized")
+        return func(*args, **kwargs)
+
     return wrapper
 
 
 def admin_required(func):
     @login_required
+    @wraps(func)
     def wrapper(*args, **kwargs):
-        if current_user.role != 'admin':
-            json_abort(403, message=['Fobidden'])
-        func(*args, **kwargs)
+        if current_user.role != "admin":
+            json_abort(403, message="Forbidden")
+        return func(*args, **kwargs)
+
     return wrapper
 
 
 def get_or_404(model, id):
     item = model.query.get(int(id))
     if item is None:
-        json_abort(404, message=['Resource not found'])
+        json_abort(404, message="Resource not found")
     return item
