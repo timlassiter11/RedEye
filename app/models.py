b4b480997e0dde775ca2d9b907e737e4253f0661
Timothy Lassiter <timlassiter11@gmail.com>
2022-06-22 14:19:17 -0400

    Added search function to flight model.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
-import datetime
 import time
-from typing import Any, Dict
+import datetime
+from typing import Any, Dict, List
 
 import jwt
 from flask import current_app, url_for
@@ -17,9 +17,9 @@ class PaginatedAPIMixin:
         data.pop("_sa_instance_state", None)
         for key, value in data.items():
             if isinstance(value, datetime.time):
-                data[key] = value.strftime('%H:%M')
+                data[key] = value.strftime("%H:%M")
             if isinstance(value, datetime.date):
-                data[key] = value.strftime('%Y-%m-%d')
+                data[key] = value.strftime("%Y-%m-%d")
 
         if "id" in data:
             data["self"] = url_for(self.__endpoint__, id=data["id"])
@@ -139,7 +139,14 @@ class Flight(PaginatedAPIMixin, db.Model):
     departure_id = db.Column(db.Integer, db.ForeignKey("airport.id"))
     arrival_id = db.Column(db.Integer, db.ForeignKey("airport.id"))
     departure_time = db.Column(db.Time, nullable=False)
+    # TODO: Do we need this? Shouldn't the arrival time be
+    # calculated based on the departure and arrival airports?
     arrival_time = db.Column(db.Time, nullable=False)
+    # TODO: Maybe this shouldn't be part of a flight
+    # Generally flights with layovers cost the same as
+    # ones without layovers so mabye the cost should be
+    # calculated based on the departing and arriving
+    # airport of the entire trip. Not per flight.
     cost = db.Column(db.Float, nullable=False)
     start = db.Column(db.Date, nullable=False)
     end = db.Column(db.Date, nullable=False)
@@ -167,6 +174,96 @@ class Flight(PaginatedAPIMixin, db.Model):
             )
 
         return data
+
+    @staticmethod
+    def search(
+        departing_airport: int,
+        final_airport: int,
+        departure_date: datetime.date,
+        num_of_passengers: int = 1,
+        max_layovers: int = 3,
+        previous_flight: "Flight" = None,
+        current_path: List["Flight"] = None,
+        all_paths: List[List["Flight"]] = None,
+    ) -> List[List["Flight"]]:
+        '''Recursive function to create flight paths from departing_airport to final_airport
+        Final return value is a list of list of flights. Each internal list of flights is a path
+        from starting from departing_airport and ending at final_airport.
+        '''
+
+        # If this is the first call to this function
+        # we need to initialize the lists.
+        if current_path is None:
+            current_path = []
+
+        if all_paths is None:
+            all_paths = []
+
+        # Limit paths to a maximum number of layovers.
+        # Add one to the layoevers since one layover
+        # means two flights. 
+        if len(current_path) > max_layovers + 1:
+            return
+
+        if previous_flight:
+            # If a previous flight was given, add it to the current path.
+            current_path.append(previous_flight)
+            # If the previous flight ended at our destination
+            # that's the end of this path. Add it to the paths.
+            if previous_flight.arrival_id == final_airport:
+                all_paths.append(current_path.copy())
+                current_path.pop()
+                return
+
+            # Use the arrival time from our previous flight
+            # to find flights we can still make.
+            departure_time = previous_flight.arrival_time
+            departure_time = datetime.datetime.combine(departure_date, departure_time)
+            # Add some buffer for layover.
+            departure_time += datetime.timedelta(minutes=45)
+            departure_time = departure_time.time()
+        else:
+            # If this is the first flight start looking for flights after 3am.
+            departure_time = datetime.time(3, 0)
+
+        # Find flights departing from our current airport.
+        query = Flight.query.filter_by(departure_id=departing_airport)
+        # Make sure flights fall within the departure date.
+        query = query.filter(Flight.start <= departure_date)
+        query = query.filter(departure_date <= Flight.end)
+        # TODO: Need to make sure all flights have enough seats for the number of passengers
+        potential_flights = query.filter(Flight.departure_time >= departure_time).all()
+        for flight in potential_flights:
+            # Make sure this flight doesn't backtrack to an airport we've already been to.
+            visited = False
+            for prev in current_path:
+                if flight.arrival_id == prev.arrival_id or flight.arrival_id == prev.departure_id:
+                    visited = True
+                    break
+
+            if visited:
+                continue
+
+            Flight.search(
+                departing_airport=flight.arrival_id, 
+                final_airport=final_airport, 
+                departure_date=departure_date, 
+                num_of_passengers=num_of_passengers,
+                max_layovers=max_layovers,
+                previous_flight=flight, 
+                current_path=current_path,
+                all_paths=all_paths
+            )
+
+        # When we exit this context it means we are done with this
+        # branch of the "tree". Remove this flight from the path.
+        if len(current_path):
+            current_path.pop()
+
+        # If we are at depth of 0 and done we should pass
+        # all the paths back to the calling function.
+        if len(current_path) == 0:
+            return all_paths
 
 
 """
