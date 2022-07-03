3155c4a1ca9e6f8b68ebd33be0e26c3db747cd08
matthew kernasovic <mattkerno@gmail.com>
2022-07-03 11:02:40 -0400

    WIP Catalog

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,6 @@
 from app.main import bp
 from flask import render_template, url_for, redirect, render_template
-
+from app.forms import AirplaneForm, AirportForm, FlightForm, UserEditForm
 
 @bp.route('/')
 def home():
@@ -14,3 +14,11 @@ def home():
 @bp.route('/contact')
 def contact():
     return render_template('main/contact.html')
+
+@bp.route('/catalog')
+def catalog():
+    form = FlightForm()
+    return render_template('main/catalog.html',
+        title='Flights',
+        form=form
+    )
