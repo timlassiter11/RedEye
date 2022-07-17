f5ca39778b8bbdde531bf998270c12450825485f
Timothy Lassiter <timlassiter11@gmail.com>
2022-07-17 07:30:08 -0400

    Created user resource base class

--- app/api/users.py
+++ app/api/users.py
@@ -11,10 +11,10 @@ from app.api.helpers import (
 from app.forms import UserEditForm
 from flask_restful import Resource, reqparse, request
 
+class UsersResource(Resource):
+    __model__ = models.User
+    __endpoint__ = "api.users"
 
-@api.resource("/customers")
-class Customers(Resource):
-    @role_required(["agent", "admin"])
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument("per_page", type=int, default=25, location="args")
@@ -26,49 +26,42 @@ class Customers(Resource):
         page = args["page"]
         search = args["search"]
 
-        query = models.Customer.query
+        query = self.__model__.query
         if search:
             query = query.msearch(f"{search}*")
 
-        data = models.User.to_collection_dict(
-            query, page, items_per_page, "api.customers", search=search
+        data = self.__model__.to_collection_dict(
+            query, page, items_per_page, self.__endpoint__, search=search
         )
         return data
 
-    @role_required(["agent", "admin"])
     def post(self):
         form = UserEditForm(data=request.json)
         if form.validate():
-            user = models.Customer()
+            user = self.__model__()
             form.populate_obj(user)
-            # Set a random password.
-            # The user will reset it later.
-            user.set_password(uuid.uuid4().hex)
             db.session.add(user)
             db.session.commit()
             db.session.refresh(user)
-            # TODO: Send set password email
             return user.to_dict(), 201
         json_abort(400, message=form.errors)
 
 
-@api.resource("/customers/<id>")
-class Customer(Resource):
-    @owner_or_role_required(["agent", "admin"])
+class UserResource(Resource):
+    __model__ = models.User
+
     def get(self, id):
-        user: models.Customer = get_or_404(models.Customer, id)
+        user: self.__model__ = get_or_404(self.__model__, id)
         return user.to_dict()
 
-    @owner_or_role_required(["agent", "admin"])
     def delete(self, id):
-        user: models.Customer = get_or_404(models.Customer, id)
+        user: self.__model__ = get_or_404(self.__model__, id)
         db.session.delete(user)
         db.session.commit()
         return "", 204
 
-    @owner_or_role_required(["agent", "admin"])
     def patch(self, id):
-        user = models.Customer = get_or_404(models.Customer, id)
+        user = self.__model__ = get_or_404(self.__model__, id)
         form = UserEditForm(data=request.json)
         
         # Fix duplicate email error message
@@ -83,174 +76,108 @@ class Customer(Resource):
         json_abort(400, message=form.errors)
 
 
+@api.resource("/customers")
+class Customers(UsersResource):
+    __model__ = models.Customer
+    __endpoint__ = "api.customers"
+
+
+    @role_required(["agent", "admin"])
+    def get(self):
+        return super().get()
+
+    @role_required(["agent", "admin"])
+    def post(self):
+        return super().post()
+
+
+@api.resource("/customers/<id>")
+class Customer(UserResource):
+    __model__ = models.Customer
+
+    @owner_or_role_required(["agent", "admin"])
+    def get(self, id):
+        return super().get(id)
+
+    @owner_or_role_required(["agent", "admin"])
+    def delete(self, id):
+        return super().delete(id)
+
+    @owner_or_role_required(["agent", "admin"])
+    def patch(self, id):
+        return super().patch(id)
+
+
 @api.resource("/agents")
-class Agents(Resource):
+class Agents(UsersResource):
+    __model__ = models.Agent
+    __endpoint__ = "api.agents"
+
     @role_required("admin")
     def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("per_page", type=int, default=25, location="args")
-        parser.add_argument("page", type=int, default=1, location="args")
-        parser.add_argument("search", location="args")
-
-        args = parser.parse_args()
-        items_per_page = args["per_page"]
-        page = args["page"]
-        search = args["search"]
-
-        query = models.Agent.query
-        if search:
-            query = query.msearch(f"{search}*")
-
-        data = models.User.to_collection_dict(
-            query, page, items_per_page, "api.agents", search=search
-        )
-        return data
+        return super().get()
 
     @role_required("admin")
     def post(self):
-        form = UserEditForm(data=request.json)
-        if form.validate():
-            user = models.Agent()
-            form.populate_obj(user)
-            # Set a random password.
-            # The user will reset it later.
-            user.set_password(uuid.uuid4().hex)
-            db.session.add(user)
-            db.session.commit()
-            db.session.refresh(user)
-            # TODO: Send set password email
-            return user.to_dict(), 201
-        json_abort(400, message=form.errors)
+        return super().post()
 
 
 @api.resource("/agents/<id>")
-class Agent(Resource):
+class Agent(UserResource):
+    __model__ = models.Agent
+
     @owner_or_role_required("admin")
     def get(self, id):
-        user: models.Agent = get_or_404(models.Agent, id)
-        return user.to_dict()
+        return super().get(id)
 
     @owner_or_role_required("admin")
     def delete(self, id):
-        user: models.Agent = get_or_404(models.Agent, id)
-        db.session.delete(user)
-        db.session.commit()
-        return "", 204
+        return super().delete(id)
 
     @owner_or_role_required("admin")
     def patch(self, id):
-        user = models.Agent = get_or_404(models.Agent, id)
-        form = UserEditForm(data=request.json)
-
-        # Fix duplicate email error message
-        if form.email.data == user.email:
-            del form.email
-
-        if form.validate():
-            form.populate_obj(user)
-            db.session.commit()
-            db.session.refresh(user)
-            return user.to_dict(), 200
-        json_abort(400, message=form.errors)
+        return super().patch(id)
 
 
 @api.resource("/admins")
-class Admins(Resource):
+class Admins(UsersResource):
+    __model__ = models.Admin
+    __endpoint__ = "api.admins"
+
     @role_required("admin")
     def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("per_page", type=int, default=25, location="args")
-        parser.add_argument("page", type=int, default=1, location="args")
-        parser.add_argument("search", location="args")
-
-        args = parser.parse_args()
-        items_per_page = args["per_page"]
-        page = args["page"]
-        search = args["search"]
-
-        query = models.Admin.query
-        if search:
-            query = query.msearch(f"{search}*")
-
-        data = models.User.to_collection_dict(
-            query, page, items_per_page, "api.admins", search=search
-        )
-        return data
+        return super().get()
 
     @role_required("admin")
     def post(self):
-        form = UserEditForm(data=request.json)
-        if form.validate():
-            user = models.Admin()
-            form.populate_obj(user)
-            # Set a random password.
-            # The user will reset it later.
-            user.set_password(uuid.uuid4().hex)
-            db.session.add(user)
-            db.session.commit()
-            db.session.refresh(user)
-            # TODO: Send set password email
-            return user.to_dict(), 201
-        json_abort(400, message=form.errors)
+        return super().post()
 
 
 @api.resource("/admins/<id>")
-class Admin(Resource):
+class Admin(UserResource):
+    __model__ = models.Admin
+
     @role_required("admin")
     def get(self, id):
-        user: models.Admin = get_or_404(models.Admin, id)
-        return user.to_dict()
+        return super().get(id)
 
     @owner_or_role_required("admin")
     def delete(self, id):
-        user: models.Admin = get_or_404(models.Admin, id)
-        db.session.delete(user)
-        db.session.commit()
-        return "", 204
+        return super().delete(id)
 
     @owner_or_role_required("admin")
     def patch(self, id):
-        user = models.Admin = get_or_404(models.Admin, id)
-        form = UserEditForm(data=request.json)
-
-        # Fix duplicate email error message
-        if form.email.data == user.email:
-            del form.email
-
-        if form.validate():
-            form.populate_obj(user)
-            db.session.commit()
-            db.session.refresh(user)
-            return user.to_dict(), 200
-        json_abort(400, message=form.errors)
+        return super().patch(id)
 
 
 @api.resource("/users")
-class Users(Resource):
+class Users(UsersResource):
     @role_required("admin")
     def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("per_page", type=int, default=25, location="args")
-        parser.add_argument("page", type=int, default=1, location="args")
-        parser.add_argument("search", location="args")
-
-        args = parser.parse_args()
-        items_per_page = args["per_page"]
-        page = args["page"]
-        search = args["search"]
-
-        query = models.User.query
-        if search:
-            query = query.msearch(f"{search}*")
-
-        data = models.User.to_collection_dict(
-            query, page, items_per_page, "api.users", search=search
-        )
-        return data
+        return super().get()
 
 @api.resource("/users/<id>")
-class User(Resource):
+class User(UserResource):
     @owner_or_role_required("admin")
     def get(self, id):
-        user: models.User = get_or_404(models.User, id)
-        return user.to_dict()
+        return super().get(id)
