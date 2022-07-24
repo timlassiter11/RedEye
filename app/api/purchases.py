0c68566b62749edc6f7c433bbe5701fd925b13eb
Timothy Lassiter <timlassiter11@gmail.com>
2022-07-24 18:15:06 -0400

    Added flight refund emails

--- app/api/purchases.py
+++ app/api/purchases.py
@@ -1,12 +1,14 @@
 from distutils.util import strtobool
-from flask_login import current_user
-from sqlalchemy import func
-from app import models, db
-from app.api import api
-from flask_restful import Resource, reqparse, request
 
+from app import db, models
+from app.api import api
 from app.api.helpers import get_or_404, json_abort, owner_or_role_required
+from app.email import send_email
 from app.forms import TransactionRefundForm
+from flask import render_template
+from flask_login import current_user
+from flask_restful import Resource, reqparse, request
+from sqlalchemy import func
 
 
 @api.resource("/purchases")
@@ -49,10 +51,10 @@ class Purchase(Resource):
 
 @api.resource("/users/<id>/purchases")
 class UserPurchases(Resource):
-    @owner_or_role_required(['admin', 'agent'])
+    @owner_or_role_required(["admin", "agent"])
     def get(self, id):
         user: models.User = get_or_404(models.User, id)
-        
+
         parser = reqparse.RequestParser()
         parser.add_argument("per_page", type=int, default=25, location="args")
         parser.add_argument("page", type=int, default=1, location="args")
@@ -78,23 +80,52 @@ def _test_owner(**kwargs):
 
 @api.resource("/purchases/<id>/refund")
 class PurchaseRefund(Resource):
-    @owner_or_role_required(['agent', 'admin'], _test_owner)
+    @owner_or_role_required(["agent", "admin"], _test_owner)
     def post(self, id):
-        purchase: models.PurchaseTransaction = get_or_404(models.PurchaseTransaction, id)
+        purchase: models.PurchaseTransaction = get_or_404(
+            models.PurchaseTransaction, id
+        )
         form = TransactionRefundForm(data=request.json)
         if form.validate():
+            fare_refund = 0
+            refunded_tickets = []
             for ticket_field in form.tickets:
                 found = False
                 for ticket in purchase.tickets:
                     if ticket.id == ticket_field.data:
                         found = True
-                        ticket.refund_timestamp = func.now()
-                        # TODO: What happens if the agent is refunding their own ticket?
-                        # Should that be logged as "refunded by"?
-                        if current_user.role == 'agent':
-                            ticket.refunded_by = current_user.id
+                        # Only refund tickets that haven't already been refunded.
+                        if ticket.refund_timestamp is None:
+                            refunded_tickets.append(ticket)
+                            ticket.refund_timestamp = func.now()
+                            fare_refund += ticket.purchase_price
+                            # TODO: What happens if the agent is refunding their own ticket?
+                            # Should that be logged as "refunded by"?
+                            if current_user.role == "agent":
+                                ticket.refunded_by = current_user.id
+
                 if not found:
                     db.session.rollback()
-                    json_abort(400, message=f"Ticket {ticket_field.data} does not belong to transaction {purchase.id}")
+                    json_abort(
+                        400,
+                        message=f"Ticket {ticket_field.data} does not belong to transaction {purchase.id}",
+                    )
             db.session.commit()
+
+            taxes_refund = (purchase.taxes / len(purchase.tickets)) * len(refunded_tickets)
+
+            text_body = render_template(
+                "email/return.txt",
+                fare_refund=fare_refund,
+                taxes_refund=taxes_refund,
+                refunded_tickets=refunded_tickets,
+            )
+            html_body = render_template(
+                "email/return.html",
+                fare_refund=fare_refund,
+                taxes_refund=taxes_refund,
+                refunded_tickets=refunded_tickets,
+            )
+            send_email("Purchase Refund", [purchase.email], text_body, html_body)
+
             return "", 204
